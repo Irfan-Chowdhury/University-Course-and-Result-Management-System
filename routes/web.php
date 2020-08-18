952b952c6b50dba84f450dec62876b669bab39d0
Irfan-Chowdhury <irfanchowdhury80@gmail.com>
2020-08-18 10:19:54 +0600

    Environment Setup & Department Setup

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,17 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard','Backend\HomeController@index')->name('dashboard');
+
+Route::group(['namespace'=>'Backend'], function () {
+        
+    Route::group(['prefix' => 'department'], function () {
+        Route::get('/','DepartmentController@index')->name('department.index');
+        Route::post('/store','DepartmentController@store')->name('department.store');
+        Route::get('/edit/{id}','DepartmentController@edit')->name('department.edit');
+        Route::post('/update/{id}','DepartmentController@update')->name('department.update');
+        Route::get('/delete/{id}','DepartmentController@destroy')->name('department.delete');
+    });
+
+});
